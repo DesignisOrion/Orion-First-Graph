93ea40fbb0c9759e9c5765dacab21ceb245dd493
DesignIsOrion <DesignIsOrion@gmail.com>
2020-08-10 07:24:28 -0400

    first commit

---
+++ graph1.py
@@ -0,0 +1,11 @@
+# Import things
+import matplotlib.pyplot as plt
+
+# allows you to see the plots in the notebook
+# %matplotlib inline
+
+# some data
+x = list(range(0, 10))
+y = list(range(-10, 0))
+
+plt.plot(x, y)
